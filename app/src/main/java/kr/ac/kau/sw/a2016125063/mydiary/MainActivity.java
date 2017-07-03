2dde3e951cc6de10efdf1c1e66db0422c78820a4
dlcskgy <hyo517@naver.com>
2017-07-03 16:42:35 +0900

    아직아무것도안했음.

---
+++ app/src/main/java/kr/ac/kau/sw/a2016125063/mydiary/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kau.sw.a2016125063.mydiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
